66fcc76d2a4d466ebab6e912be2ba5abcb5c2c9b
Unknown <jackchiarello@gmail.com>
2017-05-03 16:06:35 -0400

    Basic timer stuff

--- Assets/Resources/Scripts/timerControlScr.cs
+++ Assets/Resources/Scripts/timerControlScr.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class timerControlScr : MonoBehaviour {
 
+    public bool mainTimerActive = true;
+    public float timerTimer = 0;
+    public float timerSecondValue = 10;
+
 	// Use this for initialization
 	void Start () {
-		
+		//StartCoroutine(WaitASecond())
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
+        if (mainTimerActive == true)
+        {
+            timerTimer = timerTimer + 1 * Time.deltaTime;
+        };
+        if(timerTimer >= 1)
+        {
+            timerTimer = 0;
+            timerSecondValue--;
+            Debug.Log(timerSecondValue);
+        }
+        	
 	}
+
+    //IEnumerator WaitASecond()
+    //{
+    //    yield return new WaitForSeconds(1);
+    //    StartCoroutine(WaitASecond());
+    //}
 }
