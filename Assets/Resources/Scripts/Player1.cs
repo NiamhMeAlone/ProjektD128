8d5696be6b4bcc9a96f90d81fa51d0b565a0a298
Unknown <teaguefcs@gmail.com>
2017-05-04 16:23:15 -0400

    Bullets and Stuff

--- Assets/Resources/Scripts/Player1.cs
+++ Assets/Resources/Scripts/Player1.cs
@@ -5,27 +5,32 @@ using UnityEngine;
 public class Player1 : MonoBehaviour {
 
     Transform t;
-    public int speed;
-
-    // Use this for initialization
+    GameObject bullet;
+    public int pSpeed;
+    public int bSpeed;
+    public float reload;
+    private float rTimer;
+    
     void Start ()
     {
         t = GetComponent<Transform>();
+        bullet = (GameObject)Resources.Load("Prefabs/P1Bullet");
+        rTimer = reload;
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
         Move();
         Rotate();
-        Debug.Log(Mathf.Floor(Input.GetAxis("FireP1H") * 10) + " " + (Input.GetAxis("FireP1V") * 10));
+        rTimer -= Time.deltaTime;
+        Debug.Log(rTimer);
 	}
     
     private void Move()
     {
         Vector2 pos = t.position;
-        pos.x += Input.GetAxis("HorizontalP1") * Time.deltaTime * speed;
-        pos.y += Input.GetAxis("VerticalP1") * Time.deltaTime * speed;
+        pos.x += Input.GetAxis("HorizontalP1") * Time.deltaTime * pSpeed;
+        pos.y += Input.GetAxis("VerticalP1") * Time.deltaTime * pSpeed;
         t.position = pos;
     }
 
@@ -41,6 +46,18 @@ public class Player1 : MonoBehaviour {
             {
                 t.rotation = Quaternion.Euler(0, 0, Vector2.Angle(new Vector2(Input.GetAxis("FireP1H"), Input.GetAxis("FireP1V")), Vector2.up));
             }
+            Fire();
+        }
+    }
+    
+    private void Fire()
+    {
+        if (rTimer <= 0)
+        {
+            Vector2 angle = new Vector2(Input.GetAxis("FireP1H"), Input.GetAxis("FireP1V"));
+            angle.Normalize();
+            Instantiate(bullet, transform.position, transform.rotation).GetComponent<Rigidbody2D>().AddForce(angle * bSpeed, ForceMode2D.Impulse);
+            rTimer = reload;
         }
     }
 }
---
+++ Assets/Resources/Scripts/Player2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+
+    Transform t;
+    GameObject bullet;
+    public int speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        t = GetComponent<Transform>();
+        bullet = (GameObject)Resources.Load("Prefabs/P1Bullet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+
+        Debug.Log(bullet);
+    }
+
+    private void Move()
+    {
+        Vector2 pos = t.position;
+        pos.x += Input.GetAxis("HorizontalP2") * Time.deltaTime * speed;
+        pos.y += Input.GetAxis("VerticalP2") * Time.deltaTime * speed;
+        t.position = pos;
+    }
+
+    private void Rotate()
+    {
+        if (!(Input.GetAxis("FireP2H") == 0 && Input.GetAxis("FireP2V") == 0))
+        {
+            if (Input.GetAxis("FireP2H") > 0)
+            {
+                t.rotation = Quaternion.Euler(0, 0, -Vector2.Angle(new Vector2(Input.GetAxis("FireP2H"), Input.GetAxis("FireP2V")), Vector2.up));
+            }
+            else
+            {
+                t.rotation = Quaternion.Euler(0, 0, Vector2.Angle(new Vector2(Input.GetAxis("FireP2H"), Input.GetAxis("FireP2V")), Vector2.up));
+            }
+        }
+    }
+}
