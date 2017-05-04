61b2765042928334d5340b3946ef78d405b24f00
Unknown <teaguefcs@gmail.com>
2017-05-04 15:12:04 -0400

    Rotation and Prefab Folder w/ Players

--- Assets/Resources/Scripts/Player1.cs
+++ Assets/Resources/Scripts/Player1.cs
@@ -17,7 +17,8 @@ public class Player1 : MonoBehaviour {
 	void Update ()
     {
         Move();
-        Debug.Log(Mathf.Floor(Input.GetAxis("HorizontalP1") * 10) + " " + (Input.GetAxis("HorizontalP1") * 10));
+        Rotate();
+        Debug.Log(Mathf.Floor(Input.GetAxis("FireP1H") * 10) + " " + (Input.GetAxis("FireP1V") * 10));
 	}
     
     private void Move()
@@ -26,5 +27,20 @@ public class Player1 : MonoBehaviour {
         pos.x += Input.GetAxis("HorizontalP1") * Time.deltaTime * speed;
         pos.y += Input.GetAxis("VerticalP1") * Time.deltaTime * speed;
         t.position = pos;
+    }
+
+    private void Rotate()
+    {
+        if (!(Input.GetAxis("FireP1H") == 0 && Input.GetAxis("FireP1V") == 0))
+        {
+            if (Input.GetAxis("FireP1H") > 0)
+            {
+                t.rotation = Quaternion.Euler(0, 0, -Vector2.Angle(new Vector2(Input.GetAxis("FireP1H"), Input.GetAxis("FireP1V")), Vector2.up));
+            }
+            else
+            {
+                t.rotation = Quaternion.Euler(0, 0, Vector2.Angle(new Vector2(Input.GetAxis("FireP1H"), Input.GetAxis("FireP1V")), Vector2.up));
+            }
+        }
     }
 }
