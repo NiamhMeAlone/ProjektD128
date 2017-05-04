903aa5f97c3c52274b68a1f09f28c53c8b87dd06
Unknown <jackchiarello@gmail.com>
2017-05-04 15:26:09 -0400

    Parallax babyyyyy

---
+++ Assets/Resources/Scripts/parallaxBg.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallaxBg : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(
+            (GameObject.Find("Player1").GetComponent<Transform>().position.x 
+            + GameObject.Find("Player2").GetComponent<Transform>().position.x) / 12,
+
+            (GameObject.Find("Player1").GetComponent<Transform>().position.y
+            + GameObject.Find("Player2").GetComponent<Transform>().position.y) / 12,
+
+            transform.position.z);
+    }
+}
